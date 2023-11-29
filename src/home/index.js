1dc04646a6bd17e9c02e0934181181a71155de3b
Hiram J. González <hiramjgonzalez98@gmail.com>
2023-11-29 19:04:26 -0400

    Add initial structure & styles

--- src/home/index.js
+++ src/home/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import classes from './styles.module.scss'
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+function Home () {
+  return (
+    <main className={classes.main}>
+      <div className={classes.wrapper}>
+        <h1 className={classes.title}>Computer Vision</h1>
+        <section className={classes.content}>
+          <article className={classes.article}>
+            <form className={classes.form}>
+              <input type='url' className={classes.input} />
+              <button type='submit'>Analyze</button>
+            </form>
+          </article>
+          <article className={classes.article}>
+            <form className={classes.form}>
+              <textarea className={classes.textarea} />
+              <button type='submit'>Generate</button>
+            </form>
+          </article>
+        </section>
+        <section className={classes.result}>
+          <h3>Result</h3>
+          <div>
+            <img src='https://via.placeholder.com/150' alt='Result' />
+          </div>
+        </section>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default Home
