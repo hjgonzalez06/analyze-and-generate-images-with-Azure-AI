775405a69d07270a370f0977dcc650a403203650
Hiram J. González <hiramjgonzalez98@gmail.com>
2023-11-30 00:41:43 -0400

    Fix some browser console warnings

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import classes from './styles/root.module.scss'
 
@@ -6,8 +6,10 @@ import App from './home'
 
 import './styles/global.scss'
 
-const root = document.getElementById('root')
+const container = document.getElementById('root')
 
-root.classList.add(classes.root)
+container.classList.add(classes.root)
 
-ReactDOM.render(<App />, root)
+const root = createRoot(container)
+
+root.render(<App />)
